035e28a024a6e80ccc62f1cdbc263fed9397bf42
Ray <rayim25@gmail.com>
2026-08-14 16:32:08 +0300

    apple options

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -66,10 +66,10 @@ class DefaultFirebaseOptions {
 
   static const FirebaseOptions ios = FirebaseOptions(
     apiKey: 'AIzaSyDp8jmhsAHZ56_6fPhShbVLSFqsUyH_qS8',
-    appId: '1:1052277485580:ios:d4525aa2439f29adb03e29',
+    appId: '1:1052277485580:ios:87de5e047f6babc7b03e29',
     messagingSenderId: '1052277485580',
     projectId: 'bims-mobile-7d6d6',
     storageBucket: 'bims-mobile-7d6d6.firebasestorage.app',
-    iosBundleId: 'com.example.bimsMobileGeneral',
+    iosBundleId: 'com.nbrb.bimsmobile',
   );
 }
